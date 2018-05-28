9a2c9ed68e42843e9981a0e980ea3feb3acd2a9d
Alain Galvan <hi@alain.xyz>
2018-05-28 19:24:46 -0400

    const char** argv love

--- src/CrossWindow/Main/NoopMain.cpp
+++ src/CrossWindow/Main/NoopMain.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char** argv)
 {
-    xwin::init(argc, argv);
-    xmain(argc, argv);
+    xwin::init(argc, (const char**)argv);
+    xmain(argc, (const char**)argv);
     return 0;
 }
