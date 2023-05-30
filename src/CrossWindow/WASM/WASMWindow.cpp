e420f1f6c9b4be23b779995683b389bd7d1ff735
Jérôme DESFIEUX <jerome.desfieux@navblue.aero>
2023-05-30 15:03:30 +0200

    S-71161 Improve WASM support

--- src/CrossWindow/WASM/WASMWindow.cpp
+++ src/CrossWindow/WASM/WASMWindow.cpp
@@ -8,18 +8,26 @@ bool Window::create(WindowDesc& desc, EventQueue& eventQueue)
 {
     mDesc = desc;
 
-    EM_ASM(var canvas = document.createElement('canvas');
-           document.body.appendChild(canvas); canvas.id = 'MainWindow';);
+    EM_ASM(
+        if(! document.getElementById('canvas'))
+        {
+            var canvas = document.createElement('canvas');
+            document.body.appendChild(canvas);
+            canvas.id = 'canvas';
+            canvas.className = 'emscripten';
+        }
+    );
 
-    emscripten_set_canvas_element_size(desc.name.c_str(), desc.width,
-                                       desc.height);
+    emscripten_set_canvas_element_size("canvas", desc.width, desc.height);
     return true;
 }
 
 void Window::close()
 {
-    EM_ASM(var canvas = document.getElementById('MainWindow');
-           document.body.removeChild(canvas););
+    EM_ASM(
+        var canvas = document.getElementById('canvas');
+        document.body.removeChild(canvas);
+    );
 }
 
 WindowDesc Window::getDesc() { return mDesc; }
